e279118d442ebc3c42d902c3abd0b4b409573bc9
Matheus Glauber <glauber.jordao1995@gmail.com>
2023-11-06 22:35:03 -0300

    Esqueleto da arquitetura criado.

---
+++ personal-financial-control-entrypoint/src/main/java/com/personalfinancialcontrolentrypoint/PersonalFinancialControlEntrypointApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinancialcontrolentrypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialControlEntrypointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialControlEntrypointApplication.class, args);
+	}
+
+}
---
+++ personal-financial-control-entrypoint/src/test/java/com/personalfinancialcontrolentrypoint/PersonalFinancialControlEntrypointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalfinancialcontrolentrypoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancialControlEntrypointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
